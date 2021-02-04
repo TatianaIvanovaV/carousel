9fc93bdc94585ab91447973d0044b08bf6c8112a
Tatiana Ivanova <komarivanova@gmail.com>
2021-02-04 22:15:28 +0300

    add nav dots

--- js/SliderCarousel.js
+++ js/SliderCarousel.js
@@ -7,8 +7,10 @@ class SliderCarousel{
     prev,
     margin = 20,
     infinity = false,
+    dots = false,
     position = 0, 
     slidesToShow = 4, 
+    responsive = [],
   }){
     this.main = document.querySelector(main);
     this.wrap = document.querySelector(wrap);
@@ -16,6 +18,7 @@ class SliderCarousel{
     this.next = document.querySelector(next);
     this.prev = document.querySelector(prev);
     this.slidesToShow = slidesToShow;
+    this.dots = document.querySelector(dots);
     this.options = {
       position,
       infinity,
@@ -23,19 +26,27 @@ class SliderCarousel{
       widthSlide: Math.floor(100 / this.slidesToShow),
       maxPosition: this.items.length - this.slidesToShow,
     }; 
+    this.responsive = responsive;
   }
   init(){
-    //console.log(this.items)
     this.addClass();
     this.addStyle();
-
+    
     if (this.prev && this.next) {
+      
       this.controlSlider();
     } else {
+      this.addDots();
       this.addArrow();
       this.controlSlider();
     }
+
+    
+
+    if ( this.responsive ) {
+    this.responsiveInit()}
   }
+
   addClass() {
     this.main.classList.add('carousel-wrap');
     this.wrap.classList.add('carousel-list');
@@ -47,9 +58,12 @@ class SliderCarousel{
     const style = document.createElement('style');
     style.id = 'carousel-style';
     style.textContent = `
+      .carousel-wrap {
+        
+      }
       .carousel-children {
-        flex: 0 0 ${this.options.widthSlide}%  !important;
-        padding: 0 ${this.options.margin}px ;
+        flex: 0 0 ${this.options.widthSlide}% !important;
+        /*mardin: 0 -${this.options.margin}px;*/
       } 
     `
     document.head.appendChild(style);
@@ -66,10 +80,10 @@ class SliderCarousel{
         this.options.position = this.options.maxPosition;
       }
 
-      /*console.log(this.options.position);
+      console.log(this.options.position);
       if (this.dotArray) {
         this.currentDot(this.options.position);
-      }*/
+      }
 
       this.wrap.style.transform = `translateX(-${
         this.options.position * this.options.widthSlide
@@ -86,10 +100,10 @@ class SliderCarousel{
       if (this.options.position > this.options.maxPosition) {
         this.options.position = 0;
       }
-      /*console.log(this.options.position);
+      console.log(this.options.position);
       if (this.dotArray) {
         this.currentDot(this.options.position);
-      }*/
+      }
 
       this.wrap.style.transform = `translateX(-${
         this.options.position * this.options.widthSlide
@@ -105,6 +119,73 @@ class SliderCarousel{
 
     this.main.appendChild(this.prev);
     this.main.appendChild(this.next);
+  }
+  addDots() {
+    if (this.dots) {
+      this.dots = document.createElement('div');
+      this.dots.className = 'carousel-dots';
+      this.main.appendChild(this.dots);
+      this.dotArray = [];
+      for (let i = 0; i < this.items.length; i++) {
+        const dot = document.createElement("span");
+        dot.classList.add("carousel-dot");
+        this.dots.appendChild(dot);
+        if (i == 0) {
+          dot.className += " carousel-dot_active";
+        }
+        this.dotArray.push(dot);
+      }
+
+      this.dotArray.forEach((item, index) => {
+        item.addEventListener("click", () => {
+          this.options.position = index;
+          console.log(index);
+          this.currentSlide(this.options.position);
+        });
+      });
+    }
+    
+  } 
+
+  currentSlide(index) {
+    this.wrap.style.transform = `translateX(-${
+      index * this.options.widthSlide
+    }%)`;
+    this.currentDot(index);
+  }
+
+  currentDot(index) {
+    for (let dot of this.dotArray) {
+      dot.classList.remove("carousel-dot_active");
+    }
+    this.dotArray[index].classList.add("carousel-dot_active");
+  }
+
+  responsiveInit() {
+    const slidesToShowDefault = this.slidesToShow; 
+    const allResponse = this.responsive.map(item => item.breakpoint);
+    const maxResponse = Math.max(...allResponse);
+
+    const checkResponse = () => {
+      const widthWindow = document.documentElement.clientWidth;
+
+      if (widthWindow < maxResponse) {
+        for (let i = 0; i < allResponse.length; i++) {
+          if (widthWindow < allResponse[i]) {
+            this.slidesToShow = this.responsive[i].slidesToShow;
+            this.options.widthSlide = Math.floor(100 / this.slidesToShow);
+            this.addStyle();
+          } 
+        }
+      } else {
+        this.slidesToShow = slidesToShowDefault;
+        this.options.widthSlide = Math.floor(100 / this.slidesToShow);
+        this.addStyle();
+      }
+    };
+    checkResponse();
+
+    window.addEventListener('resize', checkResponse)
   }
 }
 export default SliderCarousel;
--- js/main.js
+++ js/main.js
@@ -5,14 +5,20 @@ import SliderCarousel from './SliderCarousel.js'
 const opotions = {
   main: '.slider__wrapper',
   wrap: '.slider__list',
-  
   infinity: true,
-  /**responsive: [
-     {
-       breakpoint: 600,
-       settings: {
-         slidesToShow: 3
-       } */
+  dots: true,
+  responsive: [
+      {
+        breakpoint: 600,
+        slidesToShow: 2,
+        //dots: true,
+      },
+      {
+        breakpoint: 300,
+        slidesToShow: 1,
+        //dots: true,
+      },
+  ]    
 }
 
 const carousel = new SliderCarousel(opotions);
